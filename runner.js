b753d9d0ae4158c56933f370aaea500ec347dd44
TheGroTesk <sashapoberezhnyjj0@gmail.com>
2020-02-01 19:54:14 +0200

    add Restaurant controller & return response with json body

--- runner.js
+++ runner.js
@@ -1,5 +1,5 @@
-import AppFactory from 'lib/AppFactory';
-import * as config from 'config/config';
+import AppFactory from './lib/AppFactory';
+import * as config from './config/config';
 
 const app = new AppFactory(config);
 
